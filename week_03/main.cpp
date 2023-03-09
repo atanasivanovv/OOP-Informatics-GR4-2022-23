1aa6fc778da2a1c8db97e18252f930d252c4210f
Ivaylo Stoyanov <ivailo.stoyanov@dreamix.eu>
2023-03-09 17:01:33 +0200

    week 3 tasks

--- week_03/main.cpp
+++ week_03/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <fstream>
 
 int main () {
-    std::cout << "Hello Practicum 3!" << std::endl; 
+    // std::ofstream outFile("rawsentence.txt");
+
+    // if (outFile.is_open()) {
+    //     // std::cout << outFile.tellp() << std::endl;
+    //     outFile << "ABCD";
+        
+    //     // outFile.seekp(2, std::ios::end);
+    //     // std::cout << outFile.tellp() << std::endl;
+    //     outFile.close();
+    // }
+
+    std::ifstream inFile("rawsentence.txt");
+    std::ofstream parsedFile("parsedsentece.txt");
+
+    if (inFile.is_open()) {
+        char symbol = ' ';
+        while (!inFile.eof()) {
+            int positionInFile = (int) inFile.tellg() + 1;
+            symbol = inFile.get();
+
+            if (positionInFile % 2 == 0) {
+                parsedFile << symbol;
+            }
+        }
+
+        // char sentence[100];
+        // while (!inFile.eof()) {
+        //     inFile.getline(sentence, 100);
+        //     std::cout << sentence << std::endl;
+        // }
+    }
+    
+
+
     return 0;
 }
