a717a34610e97de24250f60c1cc267ff067add18
Nakahotty <naskuuu123@gmail.com>
2023-05-08 13:09:28 +0300

    add week_11

--- week_10/main.cpp
+++ week_10/main.cpp
@@ -1,18 +1,23 @@
 #include "vector/Vector.h"
 
-int main() {
+int main()
+{
     Vector v;
     v.pushBack(5);
-        // std::cout << v[56];
+    // std::cout << v[56];
 
-    try {
+    try
+    {
         std::cout << v[56];
     }
-    catch (const int& num) {
+    catch (const int &num)
+    {
         std::cerr << "THERE WAS A SECOND ERROR BUT PROGRAM STILL WORKING!";
-    } catch (...) {
+    }
+    catch (...)
+    {
         std::cout << "THERE WAS AN ERROR BUT PROGRAM STILL WORKING!";
-    } 
+    }
 
     // Implement String as well :)
 }
