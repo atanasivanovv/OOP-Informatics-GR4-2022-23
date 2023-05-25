ff3044090d48a0c453a9166c4b6fac3ff8b953e7
Atanas Ivanov <naskuuu123@gmail.com>
2023-05-25 13:40:20 +0300

    add week_13

---
+++ week_13/figures.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// масив от фигури (абстрактно)
+// void printArea(Figure* figures[]) {
+//     int size = 3;
+//     for (int i = 0; i < size; i++) {
+//         double area = figures[i]->area();
+//         std::cout << area << std::endl;
+//     }
+// }
+
+int main()
+{
+    // Figure* fig1 = new Rectangle(10, 20);
+    // Figure* fig2 = new Circle(12);
+    // Figure* fig3 = new Circle(155);
+
+    // printArea(figures) - HOW?
+}
