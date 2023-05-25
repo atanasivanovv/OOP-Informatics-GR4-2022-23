161466e8c141d402d57f61ce932092d7687f510a
Atanas Ivanov <naskuuu123@gmail.com>
2023-05-25 17:16:18 +0300

    add tasks from class

--- week_13/figures.cpp
+++ week_13/figures.cpp
@@ -1,19 +1,95 @@
 #include <iostream>
+#include <cstring>
+
+class Figure
+{
+public:
+    char *name;
+
+    // helpful if there are more fields to set in base class
+    Figure(const char *name)
+    {
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+    }
+
+    const char *getName()
+    {
+        return this->name;
+    }
+
+public:
+    virtual double perimeter() = 0;
+    virtual double area() = 0;
+};
+
+class Rectangle : public Figure
+{
+private:
+    double width, height;
+
+public:
+    Rectangle(double width, double height) : Figure("Rectangle")
+    {
+        this->width = width;
+        this->height = height;
+    }
+
+    double perimeter() override
+    {
+        return 2 * height + 2 * width;
+    }
+
+    double area() override
+    {
+        return height * width;
+    }
+};
+
+class Circle : public Figure
+{
+private:
+    double radius;
+
+public:
+    Circle(double radius) : Figure("Circle")
+    {
+        this->radius = radius;
+    }
+
+    double perimeter() override
+    {
+        return 2 * 3.14 * radius;
+    }
+
+    double area() override
+    {
+        return 3.14 * radius * radius;
+    }
+};
 
 // масив от фигури (абстрактно)
-// void printArea(Figure* figures[]) {
-//     int size = 3;
-//     for (int i = 0; i < size; i++) {
-//         double area = figures[i]->area();
-//         std::cout << area << std::endl;
-//     }
-// }
+void printArea(Figure **figures, int size)
+{
+    std::cout << size;
+    for (int i = 0; i < size; i++)
+    {
+        double area = figures[i]->area();
+        std::cout << area << std::endl;
+    }
+}
 
 int main()
 {
-    // Figure* fig1 = new Rectangle(10, 20);
-    // Figure* fig2 = new Circle(12);
-    // Figure* fig3 = new Circle(155);
+    Figure **figures = new Figure *[3];
+    figures[0] = new Rectangle(10, 20);
+    figures[1] = new Circle(12);
+    figures[2] = new Circle(155);
 
-    // printArea(figures) - HOW?
+    // dynamic
+    // Vector<Figure*> figures;
+    // Figure* figure = new <type>;
+    // figures.push_back(figure);
+
+    printArea(figures, 3);
 }
--- week_13/main.cpp
+++ week_13/main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
 
+class Base
+{
+public:
+    virtual void print()
+    {
+        std::cout << "print base class\n";
+    }
+
+    void show()
+    {
+        std::cout << "show base class\n";
+    }
+};
+
+class PureBase
+{
+public:
+    virtual void print() = 0;
+    virtual void show() = 0;
+};
+
+class Derived : public PureBase
+{
+public:
+    void print() override
+    {
+        std::cout << "print derived class\n";
+    }
+
+    void show()
+    {
+        std::cout << "show derived class\n";
+    }
+};
+
 int main()
 {
+    PureBase *bptr;
+    Derived d;
+    bptr = &d;
+
+    // run time
+    bptr->print();
+
+    // compile time
+    bptr->show();
+
+    PureBase *pure;
 }
